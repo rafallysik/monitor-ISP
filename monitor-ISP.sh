47b691f91d25f2e095e9d8d5d66f0c484c815d6c
rafallysik <40183692+rafallysik@users.noreply.github.com>
2023-11-16 17:34:09 +0000

    Create monitor-ISP.sh
    main script

---
+++ monitor-ISP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "ISP Monitoring "
+HOST_COB="x.x.x.x"  #put public IP your Internet provider
+if ping $HOST_COB -c 1 1>/dev/null 2>/dev/null  #try 1 ping
+then
+echo "ok, isp is responding"
+curl -fsS -m 3 -o /dev/null --data-raw "ISP is working" https://hc-ping.com/aaa-bbb-ccc #not commom option, IT admin often wants to get notified when outage is occuring
+else
+echo "not ok,isp is not working"
+curl -fsS -m 3 -o /dev/null --data-raw "ISP is not working" https://hc-ping.com/7e6162ac-8313-4031-a8d4-559d395daa60/fail
+fi
